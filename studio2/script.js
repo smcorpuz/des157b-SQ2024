7d10fe71b9f60d748a388adbf6a0dd062e97128b
Shelby Corpuz <smcorpuz@ucdavis.edu>
2024-05-06 20:52:17 -0700

    check yourself final

--- studio2/script.js
+++ studio2/script.js
@@ -14,7 +14,7 @@
 
     function outputHTML(data){
         let html = `<p>`;
-        html += `On ${data.point1.Date} I listened to music for ${data.point1.Minutes} minutes and my top genre was ${data.point1.Genre}`;
+        html += `On`;
         html += `</p>`;
         return html;
     }
@@ -27,6 +27,19 @@
             html += `<option value="${eachPoint}">${data[eachPoint].Date}</option>`;
         });
         return html;
+    }
+
+    document.querySelector('#selector').addEventListener('change', function(){
+        const newValue = this.value;
+        updateInterface(newValue, globalData);
+    });
+
+    function updateInterface(value, jsonData){
+        let html = `<p>`;
+        html += `I listened to music for <span id="feeling">${jsonData[value].Minutes}</span> minutes and my most listened to genre was <span id="feeling">${jsonData[value].Genre}</span>. I was feeling  `;
+        html += `<span id="feeling">${jsonData[value].Feeling}</span>`;
+        html += '</p>';
+        document.querySelector('#result').innerHTML = html;
     }
 
     getData();
