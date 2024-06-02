9c3fa69c21c401989e73facbdf70b90910016b28
Shelby Corpuz <smcorpuz@ucdavis.edu>
2024-06-01 18:04:11 -0700

    version one

--- capstone/development/version1/script.js
+++ capstone/development/version1/script.js
@@ -1,6 +1,21 @@
 (function(){
     'use strict';
 
+    //create the begnning overlay
+
+    const intro = document.getElementById('intro');
+    const close = document.querySelector('#intro button');
+        
+
+    close.addEventListener('click', function(){
+        intro.style.display = 'none';
+        document.body.classList.remove('intro-visible');
+    });
+
+
+})();
     //initilize animate on scroll
     AOS.init();
-})()
+
+    //create the overlay for the intro
+    
